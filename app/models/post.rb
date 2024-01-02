523aefd66a072e68e96efa69c78b8bbb1dfd56b6
Tim <todongoonditi@gmail.com>
2024-01-03 02:25:26 +0300

    post model edited

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+    belongs_to :user
+
+    # has_many :comments
+
+    validates :title, presence: true, uniqueness: true
+    validates :image, presence: true
+    validates :content, presence: true
+    validates :user_id, presence: true
 end
