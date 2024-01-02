643bb8e77c597bc6511825efe57a95997e723d4c
Tim <todongoonditi@gmail.com>
2024-01-03 02:27:18 +0300

    post controller edited

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,51 @@
 class PostsController < ApplicationController
+    def index
+        posts = Post.all
+        render json: posts.as_json(include: :user)
+    end
+    
+    def show
+        post = Post.find_by(id: params[:id])
+        if post
+            render json: post.as_json(include: :user)
+        else
+            render json: {error: "post not found"}, status: :not_found
+        end        
+    end
+    
+    def create
+        current_user = User.find_by(id:  session[:user_id])
+        if current_user.is_admin==true
+        
+        post = @current_user.posts.create(title: params[:title], image: params[:image], content: params[:content])
+        if post.valid?
+            render json: {success: "Post created successfully"}
+        else
+            render json: {error: post.errors.full_messages}, status: :unprocessable_entity
+        end
+        else  
+            render json: {error: "admins can only perform such operation"}
+        end             
+    end
+    
+    def update
+        post = Post.find_by(id: params[:id])
+
+        if post
+            post.update(title: params[:title], image: params[:image], content: params[:content], user_id: params[:user_id])
+            render json: {success: "post updated successful"}, status: :created
+        else
+            render json: {error: "post not found"}, status: :not_found
+        end
+    end
+    
+    def destroy
+        post = Post.find_by(id: params[:id])
+        if post
+            post.destroy
+            render json: {success: "post deleted successful"}, status: :created
+        else
+            render json: {error: "post you are trying to delete does not exist"}, status: :not_found
+        end        
+    end    
 end
