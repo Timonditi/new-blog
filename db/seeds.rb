bea63390dc3d57e621f57bd57ebdd23b98da969c
Tim <todongoonditi@gmail.com>
2024-01-03 02:23:51 +0300

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,177 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Create posts
+user = User.first
+
+posts_data =[
+  {
+    title: "Shujaa, Lionesses opponents confirmed for Challenger series opener",
+    image: "https://www.kru.co.ke/wp-content/uploads/2023/09/Shujaaaaa-scaled.jpg",
+    content: %{
+      Shujaa and Lionesses have learnt their opponents for the World Rugby Challenger Series opening round taking place in Dubai, United Arab Emirates from 12-14 January 2024.
+  
+    Shujaa will face regional rivals Uganda, Germany and Mexico in Pool C of the men’s competition while the Lionesses will play out of Pool B in the women’s competition alongside China, Czech Republic and Mexico.The World Rugby Sevens Challenger provides a promotion pathway to SVNS 2025
+  
+    Top four placed men’s and women’s teams will secure their place in the new promotion and relegation play-off competition at the SVNS Grand Final in Madrid
+    
+    The 12-team men’s and women’s competitions replicate the Olympic Games format
+    
+    The 2024 edition runs from January to May and features combined men’s and women’s events in Dubai, UAE and Montevideo, Uruguay before standalone events in Germany and Poland for men and women respectively
+    
+    Dubai Challenger Series
+    
+    Men’s pools:
+    
+    Pool A: Uruguay, Hong Kong China, Papua New Guinea, Georgia
+    
+    Pool B: Japan, Tonga, Chile, Portugal
+    
+    Pool C: Kenya, Germany, Uganda, Mexico
+    
+    Women’s pools:
+    
+    Pool A: Belgium, Thailand, Papua New Guinea, Uganda
+    
+    Pool B: China, Czech Republic, Mexico, Kenya
+    
+    Pool C: Poland, Hong Kong, Paraguay, Argentina
+    }
+  },
+  {
+    title: "Impala begin KRU Championship campaign on a high",
+    image: "https://i0.wp.com/www.kenyacup.co.ke/wp-content/uploads/2022/12/Impala-Championship.jpg?w=1920&ssl=1",
+    content: %{
+      Former Kenya Cup champions Impala started their quest for a return to the top tier with 39-5 victory over USIU Martials in KRU Championship action on Saturday 2 December 2023 at the Impala Club.
+  
+    Winger Quinto Ongo weighed in with 19 points off 2 tries, 1 penalty and 2 conversion with Milton Omondi, Cosmas Valentino, Brian Ochieng and Tyson Okoth landing a try each as USIU managed the solitary unconverted try.
+    
+    The result sees Impala top of the standings on 5 points with Daystar Falcons, UoE Trojans, South Coast Pirates and Masinde Muliro rounding off the top five.
+    
+    Daystar Falcons were 34-5 winners over Mombasa as UoE Trojans and South Coast Pirates were awarded walkovers against Kabarak University and Western Bulls respectively.
+    
+    Masinde Muliro were made to work for the 36-17 victory over JKUAT Cougars.
+    
+    Mean Machine were not in action this past weekend.
+    }
+  },
+  {
+    title: "Kenya Rugby Referees elect new office bearers",
+    image: "https://www.kru.co.ke/wp-content/uploads/2021/11/Referees-768x372.jpg",
+    content: %{
+      The Kenya Rugby Referee Association, an affiliate body of the Kenya Rugby Union, convened its annual Governing Council meeting on November 16th, 2023. One of the primary items on the agenda was to elect members to the positions that were stipulated in the Association’s constitution.
+    Three candidates vied for the president position, with Alfred Okwemba emerging victorious with eight votes, earning him a four-year term. Dennis Wachira garnered seven votes, whereas Edward Mbugua received no votes.
+    The Secretary-General position also had three candidates, and Victor Oduor emerged as the winner with eight votes, earning a four-year term. Innocent Kimathi obtained seven votes, while Collins Nabiswa received no votes.
+    Two candidates contested the Honorary Treasurer position, with Alice Zawadi Mulewa retaining her position with ten votes and earning a four-year term. Edwin Kagota received five votes.
+    In addition to these, there were appointments that saw the retention of Constant Cap as a technical member and the appointment of three trustees, namely Damas Ogwe, Lameck Ongati, and James Njeru. KRU Director Leslie Mwangale was in attendance at the meeting.
+    } 
+  },
+  {
+    title: "Germany Land In Nairobi Ahead Of Tatu City Series Clash",
+    image: "https://img2.thejournal.ie/inline/3283950/original/?width=630&version=3283950",
+    content: %{
+      Germany arrived in the country on Tuesday evening ahead of Saturday’s Tatu City Series test against Kenya at the RFUEA Ground on Saturday 27 May 2017.
+  
+    The side, ranked 24th in the world, just a position below Kenya’s Simbas will hold their first training session in Nairobi on Wednesday, with their eyes trained on Saturday’s fixture which marks the start of Kenya’s international assignments for 2017.
+    
+    German squad to Nairobi
+    
+    Sean Armstrong, Raynor Parkinso, Timo Vollenkemper, Kehoma Brenner, Jaco Otto, Jarrid Els, Harris Aounallah, Dale Garner, Michael Poppmeier, Jörn Schröder, Pirmin Stöhr, Luis Ball, Antony Dickinson, Chris Howells, Ben Ellermann, Daniel Cook, Jamie Murphy, Marcel Coetzee, Erik Marks, Damien Tussac, Tim Menzel, Mikael Tyumenev, Julius Nostadt, Christopher Hilsenbeck, Dasch Barber, Samy Füchsel, Mathieu Ducau, Felicien Vergnon, Marcel Henn.
+    }
+  },
+  {
+    title: "Nondies rally to register first win of the season",
+    image: "https://i0.wp.com/www.kenyacup.co.ke/wp-content/uploads/2022/12/Nondies-2022-Championship.jpg?w=1920&ssl=1",
+    content: %{
+          Trailing 16-11 early in the second half, Nondescript scored and converted three second half tries to register their first win of the season courtesy of the 32-16 result over visiting Kisumu.
+
+    Kisumu’s Washington Ochieng and Nondies Calvin Sheunda traded penalties, the match finely poised at 6-3 courtesy of Ochieng’s success rate from the kicking tee in this match day three clash at the RFUEA.
+
+    Bruce Odhiambo then grounded the 100th try of the new season to give Nondies an 8-6 lead before Ochieng and Sheunda again traded penalties, Nondies taking the 11-9 lead into the interval.
+
+    Kenya U20 winger Alvin Mwamburi scored after the restart, Ochieng converting to see Kisumu lead 16-11 before collapsing under the weight of relentless Nondies pressure.
+
+    Sheunda would convert tries from Ian Mumbwani, Oliver Tambo and Ben Saleh to claim the bonus point victory for Nondies
+    }
+  },
+  {
+    title: "Heathens sign Scott Oluoch from Kenya Harlequins",
+    image: "https://scrummage.co.ke/wp-content/uploads/2022/05/download.jpg",
+    content: %{
+      Ugandan Giants Heathens have confirmed the capture of backrower  Scott George Olouch from Kenya Cup side Kenya Harlequins.
+  
+    Olouch returns to Uganda after nine months of stay with the Ngong Road Based side. The backrower was signed by immediate former  Quins Head Coach Charles Cardovillis in the December 2018 mid transfer window.
+    
+    He went to help the side reach Kenya Cup and Enterprise Cup semis. The Ugandan side confirmed Olouch return through their official social media pages.
+    }
+  }
+]
+
+posts_data.each do |data|
+    post = Post.create(
+      title: data[:title],
+      image: data[:image],
+      content: data[:content],
+      user_id: user,  # Use the dynamic user ID
+      created_at: Time.current,
+      updated_at: Time.current
+    )
+end
+
+
+# user seed
+# Use BCrypt to hash passwords
+hashed_password_tim = BCrypt::Password.create('timodongo')
+hashed_password_flo = BCrypt::Password.create('16nov1995!')
+hashed_password_juliet = BCrypt::Password.create('kolombo')
+hashed_password_debi = BCrypt::Password.create('debijack')
+hashed_password_maryann = BCrypt::Password.create('paxilee')
+
+# Create users with hashed passwords
+User.create!(
+  username: "Tim",
+  email: "todongoonditi@gmail.com",
+  profile_image: "https://avatars.githubusercontent.com/u/92398036?v=4",
+  password_digest: hashed_password_tim,
+  is_admin: true,
+  created_at: Time.current,
+  updated_at: Time.current
+)
+
+User.create!(
+  username: "flo",
+  email: "atienofloonditi@gmail.com",
+  profile_image: "",
+  password_digest: hashed_password_flo,
+  is_admin: true,
+  created_at: Time.current,
+  updated_at: Time.current
+)
+
+User.create!(
+  username: "juliet",
+  email: "akothjulietonditi@gmail.com",
+  profile_image: "",
+  password_digest: hashed_password_juliet,
+  is_admin: true,
+  created_at: Time.current,
+  updated_at: Time.current
+)
+
+User.create!(
+  username: "debi",
+  email: "debijack@gmail.com",
+  profile_image: "",
+  password_digest: hashed_password_debi,
+  is_admin: true,
+  created_at: Time.current,
+  updated_at: Time.current
+)
+
+User.create!(
+  username: "maryann",
+  email: "maryannonditi@gmail.com",
+  profile_image: "",
+  password_digest: hashed_password_maryann,
+  is_admin: true,
+  created_at: Time.current,
+  updated_at: Time.current
+)
+
