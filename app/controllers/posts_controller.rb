7e5b375a12b6ead98685389ea7c2a208ce85bcf9
Tim <todongoonditi@gmail.com>
2024-01-03 02:31:34 +0300

    controllers edited done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    skip_before_action :authorize, only: [:index]
+
     def index
         posts = Post.all
         render json: posts.as_json(include: :user)
