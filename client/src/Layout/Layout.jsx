cd28acb3159b8fb611b835b48c78a297124330b9
Tim <todongoonditi@gmail.com>
2024-01-03 03:32:02 +0300

    Layout done

--- client/src/Layout/Layout.jsx
+++ client/src/Layout/Layout.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Navbar from './Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
 
 export default function Layout() {
   return (
-    <div>Layout</div>
+    <div>
+        <Navbar />
+        <div className='container mx-auto min-h-[85vh] bg-gray-200 p-5'>
+        <Outlet />
+        </div>
+
+        <Footer />
+    </div>
   )
 }
