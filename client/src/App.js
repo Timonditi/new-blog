ac5e9f33d7d8cabd15d3c24d1d64be28edf6191b
Tim <todongoonditi@gmail.com>
2024-01-03 03:28:48 +0300

    footer done

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,12 @@ import "flowbite"
 import "./index.css"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './Layout/Layout';
+import Login from './Pages/Login';
+import Home from './Pages/Home';
+import Profile from './Pages/Profile';
+import Register from './Pages/Register';
+import Singlepost from './Pages/Singlepost';
+import Addpost from './Pages/Addpost';
 
 function App() {
   return (
@@ -10,7 +16,12 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
-
+        <Route index element={<Home />}/>
+          <Route path='Login' element={<Login />}/>
+          <Route path='Profile' element={<Profile />} />
+          <Route path='Register' element={<Register />} />
+          <Route path='post/:id' element={<Singlepost />} />
+          <Route path='Addpost' element={<Addpost />} />
         </Route>
       </Routes>
       </BrowserRouter>
