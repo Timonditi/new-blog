744dc828b23188d4336bfbd5c78affd236f8438a
Tim <todongoonditi@gmail.com>
2024-01-03 03:21:05 +0300

    dependancies updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-
+import React from 'react'
+import "flowbite"
+import "./index.css"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './Layout/Layout';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
