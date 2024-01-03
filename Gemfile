a1a4b73ed3f0ff0fa6fe6fdc89ab81d5e759eb46
Tim <todongoonditi@gmail.com>
2024-01-03 13:56:45 +0300

    changed to postgresql

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ ruby "3.2.1"
 
 gem 'rack-cors'
 
+gem "pg"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.2"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_02_230950) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "image"
     t.text "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
